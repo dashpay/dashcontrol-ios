8f3937004d9640b5e8a3ba1a587d89459182e453
Samuel Westrich <s.westrich@pencho.com.cn>
2017-08-03 06:16:54 +0700

    Price code from dashwallet

---
+++ DashControl/DCDataManager.h
@@ -0,0 +1,20 @@
+//
+//  DCDataManager.h
+//  DashControl
+//
+//  Created by Sam Westrich on 8/3/17.
+//  Copyright © 2017 dashfoundation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DCDataManager : NSObject
+
+@property (nonatomic, copy) NSString * _Nullable localCurrencyCode; // local currency ISO code
+@property (nonatomic, readonly) NSNumber * _Nullable bitcoinDashPrice; // exchange rate in bitcoin per dash
+@property (nonatomic, readonly) NSNumber * _Nullable localCurrencyBitcoinPrice; // exchange rate in local currency units per bitcoin
+@property (nonatomic, readonly) NSNumber * _Nullable localCurrencyDashPrice;
+@property (nonatomic, readonly) NSArray * _Nullable currencyCodes; // list of supported local currency codes
+@property (nonatomic, readonly) NSArray * _Nullable currencyNames; // names for local currency codes
+
+@end
---
+++ DashControl/DCWalletConstants.m
@@ -0,0 +1,11 @@
+//
+//  DCWalletConstants.m
+//  DashControl
+//
+//  Created by Sam Westrich on 8/3/17.
+//  Copyright © 2017 dashfoundation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NSString* const BRWalletBalanceChangedNotification =        @"BRWalletBalanceChangedNotification";
