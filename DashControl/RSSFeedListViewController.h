b2bff058d7770e9b2c9f90d2d0c37593fbe13c29
Manuel Boyer <boyermanuel@gmail.com>
2017-08-31 15:21:13 +0700

    Touch me harder

--- DashControl/RSSFeedListViewController.h
+++ DashControl/RSSFeedListViewController.h
@@ -9,12 +9,15 @@
 #import <UIKit/UIKit.h>
 #import <SafariServices/SafariServices.h>
 
-@interface RSSFeedListViewController : UIViewController <NSFetchedResultsControllerDelegate, UISearchControllerDelegate, UISearchResultsUpdating, SFSafariViewControllerDelegate>
+@interface RSSFeedListViewController : UIViewController <NSFetchedResultsControllerDelegate, UISearchControllerDelegate, UISearchResultsUpdating, SFSafariViewControllerDelegate, UIViewControllerPreviewingDelegate>
 
 @property (nonatomic,strong) NSManagedObjectContext* managedObjectContext;
 @property (nonatomic, retain) NSFetchedResultsController *fetchedResultsController;
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (nonatomic, strong) UISearchController *searchController;
+
+//3D Touch
+@property (nonatomic, strong) id previewingContext;
 
 @end
