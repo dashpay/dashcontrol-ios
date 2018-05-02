a720c587ba69f35af93b45edf48949dc688e2c5f
Andrew Podkovyrin <podkovyrin@gmail.com>
2018-05-02 10:19:40 +0300

    Use SDWebImage built-in animations

--- DashControl/Sources/UI/News/Views/NewsTableViewCell.m
+++ DashControl/Sources/UI/News/Views/NewsTableViewCell.m
@@ -18,6 +18,7 @@
 #import "NewsTableViewCell.h"
 
 #import <SDWebImage/UIImageView+WebCache.h>
+#import <SDWebImage/UIView+WebCache.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
@@ -30,6 +31,12 @@ NS_ASSUME_NONNULL_BEGIN
 @end
 
 @implementation NewsTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    
+    self.newsImageView.sd_imageTransition = SDWebImageTransition.fadeTransition;
+}
 
 - (void)setHighlighted:(BOOL)highlighted animated:(BOOL)animated {
     [super setHighlighted:highlighted animated:animated];
@@ -44,27 +51,8 @@ NS_ASSUME_NONNULL_BEGIN
                   imageURL:(NSURL *_Nullable)imageURL {
     self.newsTitleLabel.text = title;
     self.newsDateLabel.text = dateString;
-    [self loadImageWithURL:imageURL];
-}
-
-- (void)loadImageWithURL:(NSURL *_Nullable)url {
     UIImage *placeholderImage = [UIImage imageNamed:@"dashLogoPlaceholder"];
-    weakify;
-    [self.newsImageView sd_setImageWithURL:url placeholderImage:placeholderImage completed:^(UIImage *_Nullable image, NSError *_Nullable error, SDImageCacheType cacheType, NSURL *_Nullable imageURL) {
-        strongify;
-        
-        if (!image) {
-            return;
-        }
-
-        [UIView transitionWithView:self.newsImageView
-                          duration:cacheType == SDImageCacheTypeNone ? 0.3 : 0.0
-                           options:UIViewAnimationOptionTransitionCrossDissolve
-                        animations:^{
-                            self.newsImageView.image = image;
-                        }
-                        completion:nil];
-    }];
+    [self.newsImageView sd_setImageWithURL:imageURL placeholderImage:placeholderImage];
 }
 
 @end
